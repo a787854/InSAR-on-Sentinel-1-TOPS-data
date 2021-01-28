7295b503cea1228f1ee05e22660ade31406ada66
a787854 <46798123+a787854@users.noreply.github.com>
2021-01-28 11:36:54 +0100

    Update Geocoreg_cuda.cu

--- Geocoreg_cuda.cu
+++ Geocoreg_cuda.cu
@@ -895,6 +895,15 @@ void GeoCoreg_warp_cuda(
 		cudaFree(d_m_azimuthTime);
 		cudaFree(d_s_azimuthTime);
 	}
+	
+	cudaDeviceSynchronize();
+	int cuda_last_error_flag = cudaGetLastError();
+
+	if (cuda_last_error_flag != 0)
+	{
+		printf("Returns an error code (%d) from GPU execution!\n Please debug it or report it to developer!\n", cuda_last_error_flag);
+		exit(0);
+	}
 
 	cudaDeviceReset();
 
